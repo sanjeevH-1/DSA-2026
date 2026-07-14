51d67b6e32fa0bd11281423f8d0ed9ce63741641
sanjeevH-1 <sanjeev.hanchinal1@gmail.com>
2026-07-14 09:45:08 +0530

    Time: 3 ms (51.85%) | Memory: 27.8 MB (79.99%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -3,39 +3,43 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-
-        # find the mid ele
-
+        # STEP 1: Find the middle of the linked list
+        # We use slow/fast pointers. By the time fast reaches the end,
+        # slow will be at the midpoint.
         slow, fast = head, head.next
         while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
+            slow = slow.next
+            fast = fast.next.next
 
-        # reverse the secinnd half
+        # STEP 2: Reverse the second half of the list
+        # 'second' is the start of the second half.
         second = slow.next
-        slow.next = None # brk the first link
-        prev, curr= None, second
+        # Break the link between the two halves to form two separate lists.
+        slow.next = None
+        
+        prev, curr = None, second
         while curr:
-            nxt=curr.next
-            curr.next=prev
-            prev = curr
-            curr = nxt
-
+            nxt = curr.next
+            curr.next = prev  # Reversing the pointer
+            prev = curr       # Move prev forward
+            curr = nxt        # Move curr forward
         
-        # merge 2 list
+        # STEP 3: Merge the two lists by interleaving nodes
+        # 'prev' is now the head of the reversed second half.
         first, second = head, prev
-        
-        while first and second:
-            tmp1,tmp2 = first.next, second.next
+        while first and second:  
+            # Temporarily store next nodes to avoid losing the rest of the lists
+            tmp1, tmp2 = first.next, second.next
+            
+            # Re-stitch: first -> second -> original_first_next
             first.next = second
-            second.next= tmp1
+            second.next = tmp1
+            
+            # Advance pointers for the next iteration
             first, second = tmp1, tmp2
-
-
-
-        
