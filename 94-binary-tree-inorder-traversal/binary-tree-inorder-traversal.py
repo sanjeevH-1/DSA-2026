8ca27c1a1ff95b59bea7b574c685bf24a0ec45e7
sanjeevH-1 <sanjeev.hanchinal1@gmail.com>
2026-07-22 10:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (26.48%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -4,23 +4,25 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res=[]
-        stack=[]
-        cur=root
+        res = []
+        stack = []
+        cur = root
 
+        # Traverse as long as there are unvisited nodes (cur) or stacked ancestors
         while cur or stack:
+            # 1. Reach the leftmost node of the current subtree
             while cur:
-                stack.append(cur)
-                cur=cur.left
+                stack.append(cur)  # Save root/parent node before moving left
+                cur = cur.left
+
+            # 2. Backtrack from the bottom left
             cur = stack.pop()
-            res.append(cur.val)
-            if cur:
-                cur=cur.right
+            res.append(cur.val)    # Process Left / Root node
+
+            # 3. Move to the right subtree
+            cur = cur.right
 
         return res
-
-
-
-        
